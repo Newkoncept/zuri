347672c24de1f4ac68edfa54bd9eaeb0811d2dc2
Taiwo Oluwagbemiga <49463561+Newkoncept@users.noreply.github.com>
2022-08-02 23:53:02 +0100

    a

--- SimpliXL/SimpliXL/settings.py
+++ SimpliXL/SimpliXL/settings.py
@@ -131,10 +131,13 @@ LOGIN_URL = 'accounts:login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = [BASE_DIR / 'staticfiles']
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
+
 
 
 # UPLOAD FILE LOCATION
