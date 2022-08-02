311c46cff8e6c53ee72385322871847926210e5c
Taiwo Oluwagbemiga <49463561+Newkoncept@users.noreply.github.com>
2022-08-02 23:47:58 +0100

    h

--- SimpliXL/SimpliXL/settings.py
+++ SimpliXL/SimpliXL/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,8 @@ LOGIN_URL = 'accounts:login'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = [BASE_DIR / 'staticfiles']
 
 
 # UPLOAD FILE LOCATION
